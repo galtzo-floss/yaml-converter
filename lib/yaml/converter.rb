da66edd4f19b4e2763004ee479bc3feae23d9b6c
Peter H. Boling <peter.boling@gmail.com>
2025-11-08 14:52:40 -0700

    🎉 Skelly

---
+++ lib/yaml/converter.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "converter/version"
+
+module Yaml
+  module Converter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/yaml/converter/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Yaml
+  module Converter
+    VERSION = "0.1.0"
+  end
+end
