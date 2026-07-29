2a9c362e8de257e2e39dc9d9f4791791dbcb932b
Peter H. Boling <peter.boling@gmail.com>
2026-07-29 00:51:47 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/yaml/converter/version_spec.rb
+++ spec/yaml/converter/version_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Yaml::Converter::Version do
   it_behaves_like "a Version module", described_class
 
   it "executes the version file for coverage without redefining constants" do
-    path = File.expand_path("../../../lib/yaml/converter/version.rb", __dir__)
-    anonymous_namespace = AnonymousLoader.load(files: path)
+    paths = [
+      File.expand_path("../../../lib/yaml/converter/version.rb", __dir__),
+      File.expand_path("../../../lib/yaml/converter/version_gem.rb", __dir__)
+    ].select { |path| File.file?(path) }
+    anonymous_namespace = AnonymousLoader.load(files: paths)
 
     expect(anonymous_namespace::Yaml::Converter::Version::VERSION).to eq(described_class::VERSION)
   end
